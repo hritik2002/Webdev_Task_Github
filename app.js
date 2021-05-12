6323ab7f3283c1f5bf134f39f9ab95b9af45cc49
hritik2002 <72138429+hritik2002@users.noreply.github.com>
2021-05-12 08:37:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+menuIcon.addEventListener('click',function(){
+    
+    if(nav.classList.contains('duplicate-ul')){
+        nav.classList.remove('duplicate-ul');
+    }
+    else
+    {
+        nav.classList.add('duplicate-ul');
+    }
+})
+
+var signbtn = document.querySelector('#sign');
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    signbtn.textContent = "Sign up your team";
+  }
+  else
+    signbtn.textContent = "Sign up your team";
+}
+
+var x = window.matchMedia("(min-width: 925px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
